def33bcd5b8618a8fb530a5ff3ca1187c9648a07
Berdeshubham <berdeshubham1@gmail.com>
2022-11-19 11:15:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React,{ useState } from 'react';
+
 
 function App() {
+  const [mode , setmode] = useState('light');
+  const[modeName,setmodeName] = useState('Enable Dark Mode')
+  
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setmode('dark');
+      setmodeName('Enable Light Mode')
+      document.body.style.backgroundColor = "#051f3aeb";
+    }
+    else{
+      setmode('light')
+      setmodeName('Enable Dark Mode')
+      document.body.style.backgroundColor = "white";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="TextUtils" abouttext="about me" mode={mode} toggleMode={toggleMode} modeName={modeName}/>
+    <div className="container my-3">  {/*my-3 is for margin*/}
+    <TextForm heading="Enter the text to analyze below" mode={mode}/>
     </div>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'  //{useState} is a hook 
+
+export default function TexForm(props){  
+    
+    const handleUpClick = ()=>{
+        let newText=text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleLoClick = ()=>{
+        let newText=text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleClearClick = ()=>{
+        let newText="";
+        setText(newText)
+    }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value)
+    }
+    
+    const handleCopy = ()=>{
+        let text = document.getElementById("myBox");
+        text.select()
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
+    const [text,setText]= useState("")  //text is a variable & setText is a method to change the valu of a variable
+                   // text="new text";  // wrong way to change the state
+                   // setText("new text"); //correct way to change the state
+    return(
+        <>
+        <div className='container' style={{color:props.mode==='dark'? 'white': 'black'}}>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: 'lightgrey'}} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn_primary mx-1" onClick={handleUpClick} style = {{backgroundColor:'grey'}}>Convert to Uppercase</button> {/** mx-1 is for spacing it is bootstrap class */}
+            <button className="btn_primary mx-1" onClick={handleLoClick} style = {{backgroundColor:'grey'}}>Convert to Lowercase</button>
+            <button className="btn_primary mx-1" onClick={handleClearClick} style = {{backgroundColor:'grey'}}>Clear Text</button>
+            <button className="btn_primary mx-1" onClick={handleCopy} style = {{backgroundColor:'grey'}}>Copy Text</button>
+            <button className="btn_primary mx-1" onClick={handleExtraSpaces} style = {{backgroundColor:'grey'}}>Remove Extra Spaces</button>
+        </div>
+        <div className='container my-3' style={{color:props.mode==='dark'? 'white': 'black'}}>
+            <h2>Your Text Summary</h2>
+            <p>{text.split(" ").length} Words and {text.length} Characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes to read</p> {/** 0.008 this valu is time to read a single word and this value comes from google */}
+            <h2>Preview</h2>
+            <p>{text.length<=0?  "Enter somthing in the textbox above to preview it here" : text}</p>
+        </div>
+        </>
+    )
+
+}
